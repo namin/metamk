dc37a2df75b1ed0fd9ef455a27fbb0001e7b3606
Nada Amin <namin@alum.mit.edu>
2013-12-04 19:37:00 +0100

    move peano example to its own file

--- meta-tests.scm
+++ meta-tests.scm
@@ -1,15 +1,6 @@
 (load "meta.scm")
+(load "ex-peano.scm")
 (load "test-check.scm")
-
-(defrel (pluso)
-   pluso-clause
-   (pluso a b c)
-   (conde
-    ((== a 'z) (== b c))
-    ((fresh (a-1 c-1)
-            (== a `(s ,a-1))
-            (== c `(s ,c-1))
-            (pluso a-1 b c-1)))))
 
 (test-check "pluso-clause"
   (run* (q)
