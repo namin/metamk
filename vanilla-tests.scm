17b6a7df5166346efa7dd6f5f370081464ed3964
Nada Amin <namin@alum.mit.edu>
2013-12-04 20:08:13 +0100

    meta STLC

--- vanilla-tests.scm
+++ vanilla-tests.scm
@@ -1,5 +1,6 @@
 (load "vanilla.scm")
 (load "ex-peano.scm")
+(load "ex-stlc.scm")
 (load "test-check.scm")
 
 (test-check "pluso-vanilla"
@@ -11,3 +12,12 @@
             '((z _.0 _.0)
               ((s z) _.0 (s _.0))
               ((s (s z)) _.0 (s (s _.0)))))
+
+(test-check "stlc-vanilla"
+            (run 4 (q) (eigen (alpha)
+                               ((vanilla !-o-clause)
+                                `(!-o () ,q (,alpha -> ,alpha)))))
+  '(((lambda (_.0) _.0) (sym _.0))
+    ((lambda (_.0) ((lambda (_.1) _.1) _.0)) (sym _.0 _.1))
+    (((lambda (_.0) _.0) (lambda (_.1) _.1)) (sym _.0 _.1))
+    ((lambda (_.0) ((lambda (_.1) _.0) _.0)) (=/= ((_.0 _.1))) (sym _.0))))
