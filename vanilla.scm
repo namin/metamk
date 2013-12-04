77e41799aa9333b11fff8f00af790758501e4d14
Nada Amin <namin@alum.mit.edu>
2013-12-04 22:54:58 +0100

    indent

--- vanilla.scm
+++ vanilla.scm
@@ -4,12 +4,12 @@
 (define (vanilla* clause)
   (define (solve* goals)
     (conde
-     ((== goals '()))
-     ((fresh (first-goal other-goals first-body)
-             (== (cons first-goal other-goals) goals)
-             (clause first-goal first-body)
-             (solve* first-body)
-             (solve* other-goals)))))
+      ((== goals '()))
+      ((fresh (first-goal other-goals first-body)
+         (== (cons first-goal other-goals) goals)
+         (clause first-goal first-body)
+         (solve* first-body)
+         (solve* other-goals)))))
   solve*)
 
 (define vanilla (one-goal vanilla*))
